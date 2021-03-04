d5a700ea844793b701f0dac57e99c5c1b4081294
Ayan Choudhary <ayanchoudhary1025@gmail.com>
2021-03-05 00:11:45 +0530

    change ui elements from HTML to spectrum and add comments to make the code more verbose

--- secure-storage-sample/index.js
+++ secure-storage-sample/index.js
@@ -1,19 +1,19 @@
 const secureStorage = require("uxp").storage.secureStorage;
-const app = window.require("photoshop").app;
-
 
 async function saveSecureKey() {
-    const value = document.getElementById("secure").value;
-    await secureStorage.setItem("secure", value);
-    document.getElementById("secure").value = ""
+    const userInput = document.getElementById("secure").value;
+    await secureStorage.setItem("secure", userInput);
+    document.getElementById("secure").value = "";
 }
 
 async function fetchSecureKey() {
-  const value = await secureStorage.getItem("secure");
-  let string = "";
-  for (let i of value) string += String.fromCharCode(i);
-  document.getElementById("display").innerHTML = `
-  <div>Secure Key entered is ${string}</div>`;
+  // We get the stored value from the secureStorage in the form of a uint8Array.
+  const uintArray = await secureStorage.getItem("secure");
+  // We convert the uint8Array to a string to present it to the user.
+  let secureKey = "";
+  for (let i of uintArray) secureKey += String.fromCharCode(i);
+  document.getElementById("display").textContent = `
+  Secure Key entered is ${secureKey}`;
 }
 
 document.getElementById("btnPopulate").addEventListener("click", saveSecureKey);
