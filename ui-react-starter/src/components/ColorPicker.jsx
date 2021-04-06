3890eabd77207615ac587b0f6ca33003b00f48ce
PragatiVerma18 <itispragativerma@gmail.com>
2021-04-06 11:40:41 +0530

    Updated to Functional Components

--- ui-react-starter/src/components/ColorPicker.jsx
+++ ui-react-starter/src/components/ColorPicker.jsx
@@ -1,114 +1,92 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import { WC } from "../components/WC.jsx";
 
 import "./ColorPicker.css";
 
-export class ColorPicker extends React.Component {
-  #sldR;
-  #sldG;
-  #sldB;
-  #txtR;
-  #txtG;
-  #txtB;
-  constructor(props) {
-    super(props);
-    this.state = {
-      r: 0xf0,
-      g: 0xc0,
-      b: 0xa0,
-    };
+export const ColorPicker = () => {
+  const _sldR = useRef(null);
+  const _sldG = useRef(null);
+  const _sldB = useRef(null);
+  const _txtR = useRef(null);
+  const _txtG = useRef(null);
+  const _txtB = useRef(null);
 
-    this.#sldR = React.createRef();
-    this.#sldG = React.createRef();
-    this.#sldB = React.createRef();
-    this.#txtR = React.createRef();
-    this.#txtG = React.createRef();
-    this.#txtB = React.createRef();
-  }
-  updateColor = (evt) => {
+  const [R, setR] = useState(0xf0);
+  const [G, setG] = useState(0xc0);
+  const [B, setB] = useState(0xa0);
+
+  const updateColor = (evt) => {
     const target = evt.target;
     const part = target.getAttribute("data-part");
-
-    this.setState({ [part]: Number(target.value) });
+    switch (part) {
+      case "R":
+        setR(Number(target.value));
+        break;
+      case "G":
+        setG(Number(target.value));
+        break;
+      case "B":
+        setB(Number(target.value));
+        break;
+      default:
+        break;
+    }
   };
-  render() {
-    const { r, g, b } = this.state;
-    return (
-      <div className="colorPicker">
-        <div
-          className="color"
-          style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}
-        ></div>
-        <WC onInput={this.updateColor}>
-          <div className="sliderWithInput">
-            <div className="filledRange">
-              <sp-slider
-                show-value="false"
-                ref={this.#sldR}
-                data-part="r"
-                value={r}
-                min={0}
-                max={255}
-              >
-                <sp-label slot="label">Red</sp-label>
-              </sp-slider>
-            </div>
-            <sp-textfield
-              ref={this.#txtR}
-              data-part="r"
-              type="number"
-              value={r}
-              min={0}
-              max={255}
-            ></sp-textfield>
+
+  return (
+    <div className="colorPicker">
+      <div
+        className="color"
+        style={{ backgroundColor: `rgb(${R}, ${G}, ${B})` }}
+      ></div>
+      <WC onInput={updateColor}>
+        <div className="sliderWithInput">
+          <div className="filledRange">
+            <sp-slider ref={_sldR} data-part="R" value={R} min={0} max={255}>
+              <sp-label slot="label">Red</sp-label>
+            </sp-slider>
           </div>
-          <div className="sliderWithInput">
-            <div className="filledRange">
-              <sp-slider
-                show-value="false"
-                ref={this.#sldG}
-                data-part="g"
-                value={g}
-                min={0}
-                max={255}
-              >
-                <sp-label slot="label">Green</sp-label>
-              </sp-slider>
-            </div>
-            <sp-textfield
-              ref={this.#txtG}
-              data-part="g"
-              type="number"
-              value={g}
-              min={0}
-              max={255}
-            ></sp-textfield>
+          <sp-textfield
+            ref={_txtR}
+            data-part="R"
+            type="number"
+            value={R}
+            min={0}
+            max={255}
+          ></sp-textfield>
+        </div>
+        <div className="sliderWithInput">
+          <div className="filledRange">
+            <sp-slider ref={_sldG} data-part="G" value={G} min={0} max={255}>
+              <sp-label slot="label">Green</sp-label>
+            </sp-slider>
           </div>
-          <div className="sliderWithInput">
-            <div className="filledRange">
-              <sp-slider
-                show-value="false"
-                ref={this.#sldB}
-                data-part="b"
-                value={b}
-                min={0}
-                max={255}
-              >
-                <sp-label slot="label">Blue</sp-label>
-              </sp-slider>
-            </div>
-            <sp-textfield
-              ref={this.#txtB}
-              data-part="b"
-              type="number"
-              value={b}
-              min={0}
-              max={255}
-            ></sp-textfield>
+          <sp-textfield
+            ref={_txtG}
+            data-part="G"
+            type="number"
+            value={G}
+            min={0}
+            max={255}
+          ></sp-textfield>
+        </div>
+        <div className="sliderWithInput">
+          <div className="filledRange">
+            <sp-slider ref={_sldB} data-part="B" value={B} min={0} max={255}>
+              <sp-label slot="label">Blue</sp-label>
+            </sp-slider>
           </div>
-        </WC>
-      </div>
-    );
-  }
-}
+          <sp-textfield
+            ref={_txtB}
+            data-part="B"
+            type="number"
+            value={B}
+            min={0}
+            max={255}
+          ></sp-textfield>
+        </div>
+      </WC>
+    </div>
+  );
+};
