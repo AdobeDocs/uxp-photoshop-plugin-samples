abef105f28b2ab1ed220a1864836c31b929e19a3
Kerri Shotts <kerrishotts@gmail.com>
2021-07-13 13:46:47 -0400

    Add re-ordering list example

--- ui-kitchen-sink/dragdrop.js
+++ ui-kitchen-sink/dragdrop.js
@@ -1,4 +1,103 @@
 {
+    /* Support list re-ordering */
+    const list = document.querySelector(".reorderable");
+
+    function dragStart(evt) {
+        const target = evt.target;
+        const dataTransfer = evt.dataTransfer;
+        dataTransfer.effectAllowed = "all";
+
+        dataTransfer.setData("text/json", JSON.stringify({
+            key: target.getAttribute("data-key")     
+        }));
+        dataTransfer.setDragImage(target);
+
+        /* create drop targets */
+        const dropTargetEl = () => {
+            const el = document.createElement("div");
+            el.className = "target";
+            return el;
+        }
+        const listEls = Array.from(list.children);
+        listEls.forEach(el => {
+            list.insertBefore(dropTargetEl(), el);
+        })
+        list.appendChild(dropTargetEl());
+    }
+
+    function dragOver(evt) {
+        if (evt.target.classList.contains("target") || evt.target.getAttribute("data-key")) {
+            evt.preventDefault();
+            evt.stopPropagation();
+            evt.dataTransfer.dropEffect = "copy";
+        }
+        if (evt.target.classList.contains("target")) {
+            evt.preventDefault();
+            evt.stopPropagation();
+            evt.target.classList.add("active");
+        } else if (evt.target.getAttribute("data-key")) {
+            evt.preventDefault();
+            evt.stopPropagation();
+            evt.target.nextElementSibling.classList.add("active");
+        }
+    }
+
+    function dragEnter(evt) {
+        if (evt.target.classList.contains("target") || evt.target.getAttribute("data-key")) {
+            evt.preventDefault();
+            evt.stopPropagation();
+        }
+    }
+
+    function dragLeave(evt) {
+        if (evt.target.classList.contains("target")) {
+            evt.preventDefault();
+            evt.stopPropagation();
+            evt.target.classList.remove("active");
+        } else if (evt.target.getAttribute("data-key")) {
+            evt.preventDefault();
+            evt.stopPropagation();
+            evt.target.nextElementSibling.classList.remove("active");
+        }
+    }
+
+    function drop(evt) {
+        const target = evt.target;
+        if (target.classList.contains("target") || target.getAttribute("data-key")) {
+            console.log(target);
+            const json = evt.dataTransfer.getData("text/json");
+            let key;
+            if (json) {
+                key = JSON.parse(json).key;
+            }
+            if (key) {
+                console.log(key)
+                const movingEl = list.querySelector(`[data-key=${key}]`);
+                if (target.nextSibling) {
+                    list.insertBefore(movingEl, target.nextSibling);
+                } else {
+                    list.appendChild(movingEl);
+                }
+            }
+        }
+    }
+
+    function dragEnd(evt) {
+        const listEls = Array.from(list.querySelectorAll(".target"));
+        listEls.forEach(el => el.remove());
+    }
+
+    list.addEventListener("dragstart", dragStart);
+    list.addEventListener("dragover", dragOver);
+    list.addEventListener("dragenter", dragEnter);
+    list.addEventListener("dragleave", dragLeave);
+    list.addEventListener("dragend", dragEnd);
+    list.addEventListener("drop", drop);
+
+}
+
+
+{
     /* Support Draggable Images */
     function dragStart(event) {
         console.log("here");
