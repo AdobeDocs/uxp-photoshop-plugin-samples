bc81a176ccb71474dd1cd36d52c777c2af2045c3
jake-pauls <jakepauls99@gmail.com>
2021-07-29 11:12:54 -0700

    Updated socket.io client urls and modified readme to reflect url requirements on localhost

--- desktop-helper-sample/uxp/src/index.js
+++ desktop-helper-sample/uxp/src/index.js
@@ -13,9 +13,7 @@ entrypoints.setup({
   },
 });
 
-let socket = io('http://localhost:4040', {
-  transports: ['websocket'],
-});
+let socket = io('http://127.0.0.1:4040');
 
 // Attempt to reconnect if server isn't running
 socket.on('connect_error', () => {
