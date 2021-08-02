461c2612e24988f0a90b9380ef681175de592919
Kerri Shotts <kerrishotts@gmail.com>
2021-08-02 17:34:42 -0400

    add video tag

--- ui-kitchen-sink/index.js
+++ ui-kitchen-sink/index.js
@@ -40,6 +40,29 @@ const reloadPlugin = () => {
   window.location.reload();
 }
 
+let playbackTimer;
+document.querySelector("#playVideo").onclick = () => {
+  document.querySelector("video").play();
+  if (playbackTimer) {
+    clearInterval(playbackTimer);
+      playbackTimer = null;
+  }
+  playbackTimer = setInterval(() => {
+    try {
+      document.querySelector("#currentTime").textContent = document.querySelector("video").currentTime;
+    } catch (err) {
+      clearInterval(playbackTimer);
+      playbackTimer = null;
+    }
+  }, 150)
+}
+
+document.querySelector("#stopVideo").onclick = () => {
+  document.querySelector("video").stop();
+  clearInterval(playbackTimer);
+  playbackTimer = null;
+}
+
 // set up entry points -- this defines the Reload Plugin handler
 // and the panel (including its associated flyout items)
 entrypoints.setup({
