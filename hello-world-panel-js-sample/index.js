d6fa286ca90456b9ff33e8fceaf7235d4cdfb380
Barkin Aygun <386714+baaygun@users.noreply.github.com>
2021-10-07 15:53:11 -0700

    Update plugins for 23.0

--- hello-world-panel-js-sample/index.js
+++ hello-world-panel-js-sample/index.js
@@ -6,8 +6,22 @@ function showLayerNames() {
     }
     const activeDocTitle = app.activeDocument.title;
     document.getElementById("layer-title").innerHTML = `Layers for document ${activeDocTitle}`;
-    const allLayers = app.activeDocument.layers;
-    const allLayerNames = allLayers.map(layer => layer.name);
+
+    // Collection classes are proxies on arrays. So if you want to push on them and alter them
+    // you need to copy them into a proper array
+    let allLayers = Array.from(app.activeDocument.layers);
+
+    const allLayerNames = [];
+    // We do not have a way to get a flat list of all layer names in a document
+    // but you can use layers and Array.reduce to recursively collect names
+    while (allLayers.length > 0) {
+      const layer = allLayers.shift();
+      allLayerNames.push(layer.name);
+      if (layer.layers) {
+        layer.layers.forEach(l => allLayers.push(l));
+      }
+    }
+
     const sortedNames = allLayerNames.sort((a, b) => a.toUpperCase() < b.toUpperCase() ? -1 : a.toUpperCase() > b.toUpperCase() ? 1 : 0);
     document.getElementById("layers").innerHTML = `
       <ul>${
