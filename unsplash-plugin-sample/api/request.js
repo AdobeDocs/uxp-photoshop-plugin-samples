b531d168191cedd647b9f7a7a1f7e8cf8248eed4
ashikka <ashikagupta28@gmail.com>
2021-10-15 10:26:12 +0530

    fix: Removed console logs

--- unsplash-plugin-sample/api/request.js
+++ unsplash-plugin-sample/api/request.js
@@ -1,7 +1,6 @@
 import { api } from "./api";
 
-export const getPhotos = async (photoName) => {
-  const res = await api.post("/", { photoName });
-  console.log(res);
-  return res.data;
+export const getPhotos = async (pictureName) => {
+  const res = await api.post("/", { pictureName });
+  return res.data.message.results;
 };
