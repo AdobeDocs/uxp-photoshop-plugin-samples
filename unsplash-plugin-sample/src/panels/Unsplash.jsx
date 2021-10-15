a72a0cc1e6ebd45d6863b507a3c41618a2fa94b7
ashikka <ashikagupta28@gmail.com>
2021-10-15 10:26:33 +0530

    feat: Added grid display system

--- unsplash-plugin-sample/src/panels/Unsplash.jsx
+++ unsplash-plugin-sample/src/panels/Unsplash.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { getPhotos } from "../../api/request";
+import "./Unsplash.css";
 
 const Unsplash = () => {
-  return <h1>Hello</h1>;
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  async function getResults(searchTerm) {
+    setSearchResults(await getPhotos(searchTerm));
+  }
+
+  return (
+    <>
+      <div>
+        <input value={searchTerm} onChange={handleChange} />
+        <button onClick={() => { getResults(searchTerm) }}>Search</button>
+      </div>
+      <div className="container">
+        {searchResults ? (searchResults.map((item) => {
+          return (
+            <div className="grid-image" style={{ backgroundImage: 'url(' + item.links.download + ')' }} />
+          )
+        })) : (<></>)}
+      </div>
+    </>
+  )
 };
 
 export default Unsplash;
