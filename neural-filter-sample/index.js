38fed55ee99a8a359eeaee84321947aa6861dbbb
Alisamar Husain <zrthxn@gmail.com>
2021-10-19 14:56:36 +0530

    create neural filter sample

--- neural-filter-sample/index.js
+++ neural-filter-sample/index.js
@@ -1,43 +1,58 @@
-// function showLayerNames() {
-//     const app = window.require("photoshop").app;
-//     const allLayers = app.activeDocument.layers;
-//     const allLayerNames = allLayers.map(layer => layer.name);
-//     const sortedNames = allLayerNames.sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
-//     document.getElementById("layers").innerHTML = `
-//       <ul>${
-//         sortedNames.map(name => `<li>${name}</li>`).join("")
-//       }</ul>`;
-// }
-
 const batchPlay = require("photoshop").action.batchPlay;
 
-const triggerNeural = async () => {
+// Register listeners for all events just for dev
+require("photoshop").app.eventNotifier = (event, descriptor) => {
+  console.log(event, JSON.stringify(descriptor, null, ' '));
+}
+
+const openNeuralFilters = async () => {
   const result = await batchPlay(
     [
       {
         _obj: "invokeCommand",
         commandID: 8820,
         kcanDispatchWhileModal: true,
-        _options: {
-          dialogOptions: "dontDisplay"
-        }
+        _isCommand: false
       },
       {
         _obj: "enterModalWorkspace",
         name: "Neural Filters",
         ID: 8821,
         kcanDispatchWhileModal: true,
-        _isCommand: false,
-        _options: {
-          dialogOptions: "dontDisplay"
-        }
-      }
+        _isCommand: false
+      },
+      // // Does not seem critical to opening the window/modal
+      // {
+      //   _obj: "toolModalStateChanged",
+      //   level: 1,
+      //   state: {
+      //     _enum: "state",
+      //     _value: "enter"
+      //   },
+      //   tool: {
+      //     _obj: "tool",
+      //     ID: "arwT",
+      //     title: "Move Tool"
+      //   },
+      //   selectedTool: {
+      //     _obj: "tool",
+      //     ID: "arwT",
+      //     title: "Move Tool"
+      //   },
+      //   kind: {
+      //     _enum: "kind",
+      //     _value: "tool"
+      //   },
+      //   kcanDispatchWhileModal: true,
+      //   _isCommand: false
+      // }
     ],{
-      "synchronousExecution": false,
-      "modalBehavior": "fail"
+      synchronousExecution: false,
+      modalBehavior: "execute"
     });
 
   console.log(result);
 }
 
-document.getElementById("btnPopulate").addEventListener("click", triggerNeural);
+// On button click, try to run BatchPlay
+document.getElementById("btnPopulate").addEventListener("click", openNeuralFilters);
