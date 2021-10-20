96dfcc1bf86bbf6b91afdb7cfb9fcb7963eda394
ashikka <ashikagupta28@gmail.com>
2021-10-20 17:08:38 +0530

    feat: Add logic for saving/opening images, clean up UI

--- unsplash-plugin-sample/src/panels/Unsplash.jsx
+++ unsplash-plugin-sample/src/panels/Unsplash.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { CloudDownload, LinkExternal, Heart, User } from '@styled-icons/boxicons-regular';
 const { localFileSystem: fs, fileTypes } = require('uxp').storage;
+const app = require('photoshop').app;
 
 import { getPhotos } from "../../api/request";
 import "./Unsplash.css";
@@ -19,21 +21,29 @@ const Unsplash = () => {
   async function downloadIt(link) {
     const image = await fetch(link);
     console.log(link);
-  
+
+    try {
       const img = await image.arrayBuffer();
-      const [file] = await fs.getFileForSaving();
-      console.log(file);
+      const file = await fs.getFileForSaving("image.png");
+      await file.write(img);
+      const currentDocument = app.activeDocument;
+      const newDocument = await app.open(file);
+      if (currentDocument) {
+        await newDocument.activeLayers[0].duplicate(currentDocument);
+        await newDocument.close();
+      }
+    } catch (e) {
       console.log(e);
+    }
 
     if (!file) {
       return;
     }
-    await file.write(img);
   }
 
   return (
     <>
-      <div>
+      <div class="search-container">
         <input value={searchTerm} onChange={handleChange} />
         <button onClick={() => { getResults(searchTerm) }}>Search</button>
       </div>
@@ -41,10 +51,27 @@ const Unsplash = () => {
         {searchResults ? (searchResults.map((item) => {
           { console.log(item.links) }
           return (
-            <div>
-              <div className="grid-image" style={{ backgroundImage: 'url(' + item.links.download + ')' }} />
-              <button data-image={item.links.download} onClick={(e) => { downloadIt(e.target.dataset.image) }}>Download</button>
-            </div>
+            <>
+              <div className="image-container">
+                <div className="grid-image" style={{ backgroundImage: 'url(' + item.links.download + ')' }} />
+                <div className="details-container">
+                  <div>
+                    Name: {item.alt_description}
+                  </div>
+                  <div>
+                    By <User className="icon" />: {item.user.username}
+                  </div>
+                  <div>
+                    Likes <Heart className="icon" />: {item.likes}
+                  </div>
+                  <div className="icons-container">
+                    <div className="icon-btn" data-image={item.links.download} onClick={(e) => { downloadIt(e.target.dataset.image) }}>Download <CloudDownload className="icon" /></div>
+                    <div className="icon-btn"><a className="link" href={item.links.html}>Link <LinkExternal className="icon" /></a></div>
+                  </div>
+                </div>
+              </div>
+              <hr />
+            </>
           )
         })) : (<></>)}
       </div>
