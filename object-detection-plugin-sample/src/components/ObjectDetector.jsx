1935f3fc5fd20bcd3fbab5459e74433bf4d73093
ashikka <ashikagupta28@gmail.com>
2021-11-22 16:55:14 +0530

    feat: Added objection detection code

--- object-detection-plugin-sample/src/components/ObjectDetector.jsx
+++ object-detection-plugin-sample/src/components/ObjectDetector.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import fs from "fs";
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+import cocoSsd from "@tensorflow-models/coco-ssd";
 
-const ObjectDetector = () =>{
-    <h1>Hello</h1>
-}
+const ObjectDetector = async () => {
+  const img = fs.readFile("../assets/image.jpg", function (err, data) {
+    if (err) throw err;
+    else return data;
+  });
+
+  const model = await cocoSsd.load();
+
+  const predictions = await model.detect(img);
+
+  console.log("Predictions: ");
+  console.log(predictions);
+
+  return <h1>{predictions}</h1>;
+};
 
 export default ObjectDetector;
