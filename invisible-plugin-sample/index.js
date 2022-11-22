b175bc2581ee2566835a4a4aa23b284c7fb3f7c6
thisismrali <ali.abdolyousefi@gmail.com>
2022-11-22 22:48:39 +0330

    Upgraded manifest

--- invisible-plugin-sample/index.js
+++ invisible-plugin-sample/index.js
@@ -1,12 +1,17 @@
-const  { entrypoints}= require("uxp");
-const core = require('photoshop').app;
+const { entrypoints } = require("uxp");
+const core = require("photoshop").app;
 
 entrypoints.setup({
-    plugin: {
-        create() {
-            console.log("Plugin has been loaded, plugin.create has been triggered.");
-            core.showAlert("Greetings from UXP invisible plugin.");
-        }
+  plugin: {
+    create() {
+      core
+        .showAlert("Greetings from UXP invisible plugin.")
+        .then(() => {
+          console.log(
+            "Plugin has been loaded, plugin.create has been triggered."
+          );
+        })
+        .catch(console.log);
     },
+  },
 });
-
